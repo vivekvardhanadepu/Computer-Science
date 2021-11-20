ced49c60631bb7551cf6f80df15e094bc92b3ac1
vivekvardhanadepu <vivekvicky839@gmail.com>
2021-11-20 15:28:19 +0530

    OOPS Cpp inheritance

--- OOPS/Employee.cpp
+++ OOPS/Employee.cpp
@@ -7,11 +7,13 @@ class AbstractEmployee{
     virtual void askForPromotion()=0; 
 };
 
-class Employee: public AbstractEmployee{
+class Employee: AbstractEmployee{
     private:
-        string name;
         string company;
         int age;
+
+    protected:
+        string name;
 
     public:
         void setName(string name){
@@ -61,14 +63,44 @@ class Employee: public AbstractEmployee{
         }
 };
 
-int main(){
-    Employee employee1 = Employee("matheew", "gogole", 25);
-    // employee1.IntroduceYourself();
-    employee1.setName("new_matheew");
-    employee1.askForPromotion();
+class Developer: public Employee{
+    public:
+        string language;
+        Developer(string name, string company, int age, string language):Employee(name, company, age){
+            this->language = language;
+        }
+        void fixBug(){
+            cout<<this->name<<" fixed bug using "<<language<<endl;
+        }
+};
 
-    Employee employee2 = Employee("cheta", "AI", 30);
+class Teacher: Employee{
+    public:
+        string sub;
+        void prepLesson(){
+            cout<<name<<" is preparing "<<sub<<endl;
+        }
+        Teacher(string name, string company, int age, string sub):Employee(name, company, age){
+            this->sub = sub;
+        }
+};
+
+int main(){
+    // Employee employee1 = Employee("matheew", "gogole", 25);
+    // employee1.IntroduceYourself();
+    // employee1.setName("new_matheew");
+    // employee1.askForPromotion();
+
+    // Employee employee2 = Employee("cheta", "AI", 30);
     // employee2.IntroduceYourself();
-    employee2.askForPromotion();
+    // employee2.askForPromotion();
+
+    Developer dev1 = Developer("salina", "Amazomn", 20, "cpp");
+    // dev1.IntroduceYourself();
+    dev1.fixBug();
+    dev1.askForPromotion();
+
+    Teacher t1 = Teacher("Jsack", "skl", 45, "coding");
+    t1.prepLesson();
     return 0; 
 }
