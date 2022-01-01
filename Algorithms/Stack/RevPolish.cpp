a7ebfe469d01a7ce5afeae4e36e57f16be164096
Vivek Vardhan Adepu <vivekvicky839@gmail.com>
2022-01-01 04:26:07 +0000

    InterviewBit: Adding RevPolish additional solution

--- Algorithms/Stack/RevPolish.cpp
+++ Algorithms/Stack/RevPolish.cpp
@@ -30,4 +30,29 @@ class Solution {
         }
 };
 
+int Solution::evalRPN(vector<string> &A) {
+    stack<int> S;
+    for (auto &token : A) {
+        if (token.size() > 1 || (token[0] >= '0' && token[0] <= '9')) {
+            S.push(atoi(token.c_str()));
+            continue;
+        }
+
+        int op2 = S.top(); S.pop();
+        int op1 = S.top(); S.pop();
+        char op = token[0];
+        int result;
+        if (op == '+') {
+            S.push(op1 + op2);
+        } else if (op == '-') {
+            S.push(op1 - op2);
+        } else if (op == '*') {
+            S.push(op1 * op2);
+        } else if (op == '/') {
+            S.push(op1 / op2);
+        }
+    }
+    return S.top();
+}
+
 // https://www.interviewbit.com/problems/evaluate-expression/
