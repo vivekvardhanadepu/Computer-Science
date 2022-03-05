569c4a05228ebc11c4db94238c0650524da9ecd6
vivekvardhanadepu <vivekvicky839@gmail.com>
2022-03-05 12:48:06 +0530

    adding InterviewBit/Arrays/MaxNonNegativeSubArray.cpp

--- Algorithms/InterviewBit/Arrays/MaxNonNegativeSubArray.cpp
+++ Algorithms/InterviewBit/Arrays/MaxNonNegativeSubArray.cpp
@@ -16,3 +16,37 @@ vector<int> Solution::maxset(vector<int> &A) {
     return a;
 }
 
+vector<int> Solution::maxset(vector<int> &A) {
+    long long int max_ending_here=0, max_so_far=0;
+    int max_length = 0, length = 0, max_index = 0;
+    int n = A.size();
+    vector<int> result;
+
+    for(int i=0;i<n;i++){
+        if(A[i]<0){
+            max_ending_here = 0;
+            length=0;
+            continue;
+        }
+        max_ending_here += A[i];
+        length++;
+        if(max_ending_here > max_so_far){
+            max_so_far = max_ending_here;
+            max_length = length;
+            max_index = i;
+        } else if(max_ending_here == max_so_far){
+            if(max_length<length){
+                max_length = length;
+                max_index = i;
+            }
+                
+        }
+    }
+
+    for(int i=max_index-max_length+1;i<=max_index;i++){
+        result.push_back(A[i]);
+    }
+    return result;
+}
+
+// https://www.interviewbit.com/problems/max-non-negative-subarray/
