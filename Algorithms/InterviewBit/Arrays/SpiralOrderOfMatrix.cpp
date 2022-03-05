57b30ea4996a8bcb4c6661386c9c990059a0191f
Vivek Vardhan Adepu <vivekvicky839@gmail.com>
2022-03-06 00:29:28 +0530

    adding InterviewBit/Arrays/MaxNonNegativeSubArray.cpp additional solution

--- Algorithms/InterviewBit/Arrays/SpiralOrderOfMatrix.cpp
+++ Algorithms/InterviewBit/Arrays/SpiralOrderOfMatrix.cpp
@@ -38,4 +38,45 @@ vector<int> Solution::spiralOrder(const vector<vector<int> > &A) {
     return B;
 }
 
+vector<int> Solution::spiralOrder(const vector<vector<int> > &A) {
+    int m = A.size(), n = A[0].size();
+    int i=0, j=0, dir=0;
+    vector<int> result;
+    if(m==1)
+        return A[0];
+    else if(n==1){
+        for(;i<m;i++)
+            result.push_back(A[i][0]);
+        return result;
+    }
+    while(i<m/2 && j<n/2){
+        if(dir==0){
+            for(int k=j;k<n-j;k++)
+                result.push_back(A[i][k]);
+            dir=1;
+        }else if(dir==1){
+            for(int k=i+1;k<m-i;k++)
+                result.push_back(A[k][n-j-1]);
+            dir=2;
+        }else if(dir==2){
+            for(int k=n-2-j;k>=j;k--)
+                result.push_back(A[m-1-i][k]);
+            dir=3;
+        }else{
+            for(int k=m-2-i;k>i;k--)
+                result.push_back(A[k][j]);
+            dir=0;
+            i++;j++;
+        }
+    }
+    if(m<=n && i==((m-1)/2)){
+        for(int k=j;k<n-j;k++)
+                result.push_back(A[i][k]);
+    }else if(n<m && j==((n-1)/2)){
+        for(int k=i;k<m-i;k++)
+            result.push_back(A[k][n-j-1]);
+    }
+    return result;
+}
 
+// https://www.interviewbit.com/problems/spiral-order-matrix-i/
